57790044cf21339d6f9f4709be4c9996e6435ffe
ArtemShnip <47022618+ArtemShnip@users.noreply.github.com>
2022-11-11 04:20:16 +0300

    add vrif

--- Assets/Scripts/FPSCounter.cs
+++ Assets/Scripts/FPSCounter.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FPSCounter : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI _fpsText;
     [SerializeField] private float _updateDelay;
+    [SerializeField] private GameObject text;
 
     private float _elapsedTime;
     
@@ -18,6 +20,7 @@ public class FPSCounter : MonoBehaviour
         {
             int currentFPS = (int) (1 / Time.unscaledDeltaTime);
             _fpsText.text = $"FPS: {currentFPS}";
+            text.GetComponent<TextMesh>().text=$"FPS: {currentFPS}";
             _elapsedTime = 0;
         }
     }
