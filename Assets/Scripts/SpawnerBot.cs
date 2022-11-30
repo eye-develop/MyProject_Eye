62405790724cf916bf04099fab522f75b9251e8c
ArtemShnip <47022618+ArtemShnip@users.noreply.github.com>
2022-11-30 19:37:48 +0300

    поправил спавнер ботов

--- Assets/Scripts/SpawnerBot.cs
+++ Assets/Scripts/SpawnerBot.cs
@@ -8,9 +8,12 @@ public class SpawnerBot : MonoBehaviour
     
     [SerializeField] private List<GameObject> spawners;
     [SerializeField] private GameObject parent;
-    [SerializeField] private GameObject prefabBot;
+    [SerializeField] private List<GameObject> prefabsBot;
+    [SerializeField] private int chanseHardBot = 30;
     [SerializeField] private int lvl = 1;
-    [SerializeField] private int countBot1lvl = 10;
+    [SerializeField] private int countBot1Lvl = 10;
+    [SerializeField] private GameObject prefabBotBoss;
+    [SerializeField] private int everyLevels = 5;
     
     private void Start()
     {
@@ -20,10 +23,29 @@ public class SpawnerBot : MonoBehaviour
     
     private IEnumerator Spawn()
     {
-        for (int i = 0; i < countBot1lvl; i++)
+        for (int i = 0; i < countBot1Lvl; i++)
         {
-            int rnd = Random.Range(0, spawners.Count-1);
-            Instantiate(prefabBot,spawners[rnd].transform.position,Quaternion.identity,parent.transform);
+            int randomSpawn = Random.Range(0, spawners.Count-1);
+            int randomBot = Random.Range(0, 101);
+
+            switch (randomBot)
+            {
+                case { } n when (n > chanseHardBot && n <= 100):
+                    Instantiate(prefabsBot[0],spawners[randomSpawn].transform.position,Quaternion.identity,parent.transform);
+                    break;
+                case { } n when (n <= chanseHardBot):
+                    Instantiate(prefabsBot[1],spawners[randomSpawn].transform.position,Quaternion.identity,parent.transform);
+                    break;
+                default:
+                    break;
+            }
+
+            if (lvl % everyLevels == 0)
+            {
+                Debug.Log("boss");
+                //Instantiate(prefabBotBoss,spawners[randomSpawn].transform.position,Quaternion.identity,parent.transform);
+            }
+                 
             yield return new WaitForSeconds(2f);
         }
     }
@@ -31,7 +53,8 @@ public class SpawnerBot : MonoBehaviour
     private void Level()
     {
         lvl += 1;
-        countBot1lvl += 2;
+        countBot1Lvl += 2;
+        chanseHardBot += 5;
         StartCoroutine(Spawn());
     }
     
@@ -42,7 +65,6 @@ public class SpawnerBot : MonoBehaviour
             yield return new WaitForSeconds(3f);
             if (parent.transform.childCount == 0)
             {
-                Debug.Log("level");
                 Level();
             }
         }
