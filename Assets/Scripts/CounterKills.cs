8b8c6d63ec0bb936af8eb3a1623c68aada228e67
ArtemShnip <47022618+ArtemShnip@users.noreply.github.com>
2023-01-26 13:44:40 +0300

    add voice

--- Assets/Scripts/CounterKills.cs
+++ Assets/Scripts/CounterKills.cs
@@ -8,18 +8,17 @@ public class CounterKills : MonoBehaviour
     [SerializeField] private GameObject killsText;
     private int kills;
 
-    private void Start()
+    private void Awake()
     {
         kills = 0;
-        killsText.GetComponent<TextMesh>().text=$"Kills: {kills}";
+            killsText.GetComponent<TextMesh>().text=$"Kills: {kills}";
         
-        Debug.Log($"kill + {kills}");
+        Debug.Log($"kill : {kills}");
     }
 
     public void Kill()
     {
-        Debug.Log($"kill + {kills}");
         kills += 1;
-        killsText.GetComponent<TextMesh>().text=$"Kills: {kills}";
+            killsText.GetComponent<TextMesh>().text=$"Kills: {kills}";
     }
 }
